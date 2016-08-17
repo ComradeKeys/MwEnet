404ed87d271c3e345ed945f3a805afd750c6ef73
Brigham Keys, Esq <bkeys@gnu.org>
2016-08-16 23:27:58 -0500

    Started working on an example, I should probably real all the source code before I do this.

--- examples/01.ChatRoom/src/client.cpp
+++ examples/01.ChatRoom/src/client.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <enetclient.h>
+#include <server.h>
+
+class ChatInterface : public mw::ServerInterface {
+  void receiveToServer(const mw::Packet& packet, int clientId) {
+
+  }
+
+  bool connectToServer(int clientId) {
+
+  }
+
+  void disconnectToServer(int clientId) {
+
+  }
+};
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello Client!" << std::endl;
+
+  ChatInterface interface;
+  mw::EnetClient client(1234, "localhost");
+  client.start();
+
+  for(int i = 0; i < 100000; ++i) {
+    interface.receiveToServer();
+  }
+
+  client.stop();
   return 0;
 }
--- examples/01.ChatRoom/src/server.cpp
+++ examples/01.ChatRoom/src/server.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <string>
 #include <enetserver.h>
+#include <server.h>
+#include <packet.h>
+
+class ChatInterface : public mw::ServerInterface {
+  void receiveToServer(const mw::Packet& packet, int clientId) {
+
+  }
+
+  bool connectToServer(int clientId) {
+
+  }
+
+  void disconnectToServer(int clientId) {
+
+  }
+};
 
 int main(int argc, char *argv[]) {
 
-  std::cout << "Hello Server!" << std::endl;
+  std::string myName = "Brigham";
+  mw::Packet packet(myName.c_str(), myName.size());
+  ChatInterface interface;
+  mw::EnetServer server(1234, interface);
+  server.start();
+  while(true) {
+    server.serverPushToSendBuffer(packet, mw::Network::RELIABLE);
+  }
+
+  server.stop();
+
   return 0;
 }

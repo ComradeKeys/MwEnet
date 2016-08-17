b1147464fd872f097ab2318d489cec5821aec5e6
Brigham Keys, Esq <bkeys@gnu.org>
2016-08-17 17:02:41 -0400

    Finished first serialization example

--- examples/02.Serialize/src/client.cpp
+++ examples/02.Serialize/src/client.cpp
@@ -3,17 +3,14 @@
 #include <sstream>
 
 struct MyClass {
+  MyClass() : x(1), y(2), z(3) {}
+
   int x, y, z;
 
   template<class Archive>
   void serialize(Archive &archive) {
     archive(x, y, z);
   }
-};
-
-struct MyData {
-  MyData() : i(1), j(2), k(3) {}
-  int i, j, k;
 };
 
 int main(int argc, char *argv[]) {
@@ -23,12 +20,16 @@ int main(int argc, char *argv[]) {
   {
     cereal::BinaryOutputArchive oarchive(ss); //create an output archive
 
-    MyData m1, m2, m3;
-    oarvhive(m1, m2, m3); // Write the data to the archive
+    MyClass m1, m2, m3;
+  std::cout << m1.x << m1.y << m1.z << std::endl;
+    oarchive(m1, m2, m3); // Write the data to the archive
+
   } //archive goes out of scope, ensuring all contents are flushed
 
   cereal::BinaryInputArchive iarchive(ss); // Create an input archive
 
-  MyData m1, m2, m3;
+  MyClass m1, m2, m3;
   iarchive(m1, m2, m3); // Read the data from the archive
+
+  std::cout << m1.x << m1.y << m1.z << std::endl;
 }

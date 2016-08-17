1f59a3cae306ea3ece44af8bb83c1929a6bb444d
Brigham Keys, Esq <bkeys@gnu.org>
2016-08-17 16:32:29 -0400

    Moved ENet to thirdparty folder

--- examples/02.Serialize/src/client.cpp
+++ examples/02.Serialize/src/client.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+//#include <>
+
+struct MyClass {
+  int x, y, z;
+
+  template<class Archive>
+  void serialize(Archive &archive) {
+    archive(x, y, z);
+  }
+};
 
 int main(int argc, char *argv[]) {
-
 
 
 }

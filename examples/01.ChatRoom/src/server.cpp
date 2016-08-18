c28224375b80392ba3d6a0bea8ebbc5a4fa0492e
Nicolás A. Ortega <nicolas.ortega.froysa@gmail.com>
2016-08-18 21:59:54 +0200

    Chat sends messages.

--- examples/01.ChatRoom/src/server.cpp
+++ examples/01.ChatRoom/src/server.cpp
@@ -3,18 +3,22 @@
 #include <enetserver.h>
 #include <server.h>
 #include <packet.h>
+#include <network.h>
+#include <vector>
+
+std::vector<mw::Packet> msgs;
 
 class ChatInterface : public mw::ServerInterface {
     void receiveToServer(const mw::Packet &packet, int clientId) {
-      puts("here");
+        msgs.push_back(packet);
     }
 
     bool connectToServer(int clientId) {
-      std::cout <<"The server got a new connection at" << clientId << std::endl;
+        std::cout << "The server got a new connection at " << clientId << std::endl;
     }
 
     void disconnectToServer(int clientId) {
-      puts("Client disconnected");
+        puts("Client disconnected");
     }
 };
 
@@ -26,7 +30,10 @@ int main(int argc, char *argv[]) {
     std::string welcomeMessage = "Welcome to the chat room example!\n";
     mw::Packet packet(welcomeMessage.c_str(), welcomeMessage.size());
     while(true) {
-      server.pushToSendBuffer(packet, mw::Network::RELIABLE);
+        if(not msgs.empty()) {
+            server.serverPushToSendBuffer(msgs.front(), mw::Network::RELIABLE);
+            msgs.erase(msgs.begin());
+        }
     }
 
     server.stop();
